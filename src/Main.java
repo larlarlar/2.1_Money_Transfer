8de1727e0ce31092281215deba0f394ecb7a3c37
Larisa Gurina <larisa.gurina.s@gmail.com>
2021-01-08 16:17:37 +0300

    -1794967296

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int remittance = 500_000_000;
+        int total = balance + remittance;
+        System.out.println("total:");
+        System.out.println(total);
+        System.out.println("done!");
+    }
+}
